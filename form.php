bd752ad6ff6ffcf86611a1ca31125474b441d757
Rovinsan <selvaratnam.rovinsan@my.sliit.lk>
2018-10-03 01:49:45 +0530

    change AJAX call

--- form.php
+++ form.php
@@ -72,7 +72,7 @@
          
 
 
-   <!-- CSRF Token retrieve | ajax call to the service.php -->
+   <!-- CSRF Token retrieve ajax call -->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
